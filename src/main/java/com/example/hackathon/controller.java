b11d223a513ba30cb3d28494191a39d9f2869c10
이준범 <jach1206@naver.com>
2024-11-22 04:09:03 +0900

    test: 테스트 코드 추가

---
+++ src/main/java/com/example/hackathon/controller.java
@@ -0,0 +1,14 @@
+package com.example.hackathon;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
