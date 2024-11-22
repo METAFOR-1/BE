dc2d1c81f07f222e68775e4fc33fe2fe60ea201c
이준범 <jach1206@naver.com>
2024-11-23 04:04:54 +0900

    feat: aws healthcheck 코드 추가

--- src/main/java/com/example/hackathon/controller.java
+++ src/main/java/com/example/hackathon/controller.java
@@ -2,19 +2,25 @@ package com.example.hackathon;
 
 import com.example.hackathon.domain.diagnosis.dto.request.RequestMentDto;
 import com.example.hackathon.domain.diagnosis.dto.response.ResponseModelDto;
+import com.example.hackathon.domain.youtube.service.YoutubeService;
 import com.example.hackathon.global.s3.service.S3Service;
+import java.io.IOException;
 import java.net.URL;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class controller {
 
     private final S3Service s3Service;
+    private final YoutubeService youtubeService;
 
     @PostMapping("/test")
     public ResponseModelDto test(@RequestBody RequestMentDto requestMentDto) {
@@ -25,8 +31,8 @@ public class controller {
         return responseModelDto;
     }
 
-    @GetMapping("/url")
-    public URL url() {
-        return s3Service.getImageUrl("승모근.jpg");
+    @GetMapping("/check")
+    public ResponseEntity<String> check(){
+        return ResponseEntity.ok("check");
     }
 }
